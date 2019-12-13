887e46d4f83a88e6987385a71079b16f372ef0d4
Никита Зернов <nazernov@edu.hse.ru>
2019-12-13 19:50:25 +0300

    Creating the new databasw

--- LibraryOfClasses/Program.cs
+++ LibraryOfClasses/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LibraryOfClasses
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
