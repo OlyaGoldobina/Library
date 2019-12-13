2ee241c514ff75d4b1bbedc4d4d1842e82cf3d14
Никита Зернов <nazernov@edu.hse.ru>
2019-12-13 15:32:34 +0300

    Добавьте файлы проекта.

---
+++ LibraryOfClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
