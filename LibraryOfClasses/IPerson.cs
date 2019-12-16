0db9890c4326f570c45897596e64a70627281731
Иван Еременко <iseremenko@edu.hse.ru>
2019-12-16 17:52:20 +0300

    cccc

---
+++ LibraryOfClasses/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryOfClasses
+{
+    interface IPerson
+    {
+       int Name { get; set; }
+    }
+}
