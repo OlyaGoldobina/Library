f2d70dbdc94063f9b7c55c79ae3bf31da1c13e3c
Никита Зернов <nazernov@edu.hse.ru>
2019-12-17 17:29:54 +0300

    New windows was created

---
+++ WPFInterface/WorkersInformation.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkersInformation.xaml
+    /// </summary>
+    public partial class WorkersInformation : Window
+    {
+        public WorkersInformation()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeWorkerInformation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
