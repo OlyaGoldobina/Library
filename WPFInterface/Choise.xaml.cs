9b5486bd9580c29496ae67079bf0ea5be3a742c4
Ольга Голдоибна <goldobinaoi1@yandex.ru>
2019-12-18 20:08:37 +0300

    WPF choise was created

--- WPFInterface/Choise.xaml.cs
+++ WPFInterface/Choise.xaml.cs
@@ -26,7 +26,63 @@ namespace WPFInterface
 
         private void Choose_Click(object sender, RoutedEventArgs e)
         {
-
+            string selectedOption = Options.SelectedItem .ToString();
+            if(selectedOption != "")
+            {
+                if(selectedOption == "System.Windows.Controls.ListBoxItem: Settings")
+                {
+                    Settings settingswindow = new Settings();
+                    settingswindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    if(selectedOption == "System.Windows.Controls.ListBoxItem: Workers")
+                    {
+                        Worker workerwindow = new Worker();
+                        workerwindow.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        if(selectedOption == "System.Windows.Controls.ListBoxItem: TimeTable")
+                        {
+                            TimeTable tablewindow = new TimeTable();
+                            tablewindow.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            if(selectedOption == "System.Windows.Controls.ListBoxItem: Films")
+                            {
+                                Films filmwindow = new Films();
+                                filmwindow.Show();
+                                this.Close();
+                            }
+                            else
+                            {
+                                if(selectedOption == "System.Windows.Controls.ListBoxItem: Profit information")
+                                {
+                                    Profit profitwindow = new Profit();
+                                    profitwindow.Show();
+                                    this.Close();
+                                }
+                                else
+                                {
+                                    if(selectedOption == "System.Windows.Controls.ListBoxItem: Tariffs")
+                                    {
+                                        Tarrifs tariffwindow = new Tarrifs();
+                                        tariffwindow.Show();
+                                        this.Close();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Choose the option, please");
         }
     }
 }
